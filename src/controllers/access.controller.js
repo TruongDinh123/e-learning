1a24662a205fb9f6503a07298d23c510717c6180
dinh <dinhtq170601@gmail.com>
2023-11-01 14:40:20 +0700

    Fix class property assignment syntax in access.controller.js

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,132 +1,64 @@
-// "use strict";
-// const AccessService = require("../services/access.service");
-// const { SuccessReponse } = require("../core/success.reponse");
-
-// class AccessController {
-//   login = async (req, res, next) => {
-//     try {
-//       new SuccessReponse({
-//         metadata: await AccessService.login(req.body),
-//       }).send(res);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   signUp = async (req, res, next) => {
-//     return res.status(201).json(await AccessService.signUp(req.body));
-//   };
-
-//   logOut = async (req, res, next) => {
-//     new SuccessReponse({
-//       message: "Logout success",
-//       metadata: await AccessService.logout(req.keyAccount),
-//     }).send(res);
-//   };
-
-//   updateUserRoles = async (req, res, next) => {
-//     try {
-//       const { userId, roleId } = req.body;
-//       const currentUserId = req.user.userId;
-
-//       const updatedUser = await AccessService.updateUserRoles({
-//         roleId,
-//         userId,
-//         currentUserId,
-//       });
-//       return res.status(200).json(updatedUser);
-//     } catch (error) {
-//       console.error(error);
-//       return res.status(500).json({ message: "Internal server error" });
-//     }
-//   };
-
-//   getAllUser = async (req, res, next) => {
-//     try {
-//       new SuccessReponse({
-//         message: "Get all user success",
-//         metadata: await AccessService.getAllUser(),
-//       }).send(res);
-//     } catch (error) {
-//       console.error(error);
-//       return res.status(500).json({ message: "Internal server error" });
-//     }
-//   };
-
-//   deleteUser = async (req, res, next) => {
-//     const { id } = req.params;
-//     new SuccessReponse({
-//       message: "Delete user success",
-//       metadata: await AccessService.deleteUser({ id }),
-//     }).send(res);
-//   };
-// }
-// module.exports = new AccessController();
-
-
 "use strict";
 const AccessService = require("../services/access.service");
 const { SuccessReponse } = require("../core/success.reponse");
 
 class AccessController {
-  constructor() {
-    this.login = async (req, res, next) => {
-      try {
-        new SuccessReponse({
-          metadata: await AccessService.login(req.body),
-        }).send(res);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    this.signUp = async (req, res, next) => {
-      return res.status(201).json(await AccessService.signUp(req.body));
-    };
-
-    this.logOut = async (req, res, next) => {
+  login = async (req, res, next) => {
+    try {
       new SuccessReponse({
-        message: "Logout success",
-        metadata: await AccessService.logout(req.keyAccount),
+        metadata: await AccessService.login(req.body),
       }).send(res);
-    };
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    this.updateUserRoles = async (req, res, next) => {
-      try {
-        const { userId, roleId } = req.body;
-        const currentUserId = req.user.userId;
+  signUp = async (req, res, next) => {
+    return res.status(201).json(await AccessService.signUp(req.body));
+  };
 
-        const updatedUser = await AccessService.updateUserRoles({
-          roleId,
-          userId,
-          currentUserId,
-        });
-        return res.status(200).json(updatedUser);
-      } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Internal server error" });
-      }
-    };
+  logOut = async (req, res, next) => {
+    new SuccessReponse({
+      message: "Logout success",
+      metadata: await AccessService.logout(req.keyAccount),
+    }).send(res);
+  };
 
-    this.getAllUser = async (req, res, next) => {
-      try {
-        new SuccessReponse({
-          message: "Get all user success",
-          metadata: await AccessService.getAllUser(),
-        }).send(res);
-      } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Internal server error" });
-      }
-    };
+  updateUserRoles = async (req, res, next) => {
+    try {
+      const { userId, roleId } = req.body;
+      const currentUserId = req.user.userId;
 
-    this.deleteUser = async (req, res, next) => {
-      const { id } = req.params;
+      const updatedUser = await AccessService.updateUserRoles({
+        roleId,
+        userId,
+        currentUserId,
+      });
+      return res.status(200).json(updatedUser);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  };
+
+  getAllUser = async (req, res, next) => {
+    try {
       new SuccessReponse({
-        message: "Delete user success",
-        metadata: await AccessService.deleteUser({ id }),
+        message: "Get all user success",
+        metadata: await AccessService.getAllUser(),
       }).send(res);
-    };
-  }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  };
+
+  deleteUser = async (req, res, next) => {
+    const { id } = req.params;
+    new SuccessReponse({
+      message: "Delete user success",
+      metadata: await AccessService.deleteUser({ id }),
+    }).send(res);
+  };
 }
 module.exports = new AccessController();
