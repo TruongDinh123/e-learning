e4d6d6573c05d1e00aaf3159ebfc0721656b4ce4
dinh <dinhtq170601@gmail.com>
2024-02-06 21:20:07 +0700

    FIX: origin

--- src/app.js
+++ src/app.js
@@ -12,8 +12,8 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL],
-    // origin: "https://e-learning-fe.onrender.com",
+    // origin: [process.env.CLIENT_URL],
+    origin: "https://e-learning-fe.onrender.com",
     // origin: "https://116.118.51.237:3000",
     // origin: "https://www.247learn.vn",
     credentials: true,
