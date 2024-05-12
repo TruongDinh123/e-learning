bb9b75640f5c63de5d3c279bb1de70313711ce9d
thuc-nguyen <hoangthuc701@gmail.com>
2024-05-13 00:12:36 +0700

    Fix decode

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,15 +4,16 @@ const _ = require("lodash");
 const { Types } = require("mongoose");
 const crypto = require('crypto');
 
-const algorithm = 'aes-256-cbc';
-const key = '12345678901234567890123456789012'; // Khóa 32 bytes cho aes-256
-const iv = '1234567890123456'; // IV 16 bytes
+const secret = 'your-secret-key';
+const secretKey = crypto.createHash('sha256').update(String(secret)).digest('base64').substr(0, 32);
+console.log("secretKey",secretKey);
 
 function encrypt(text) {
-  let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), Buffer.from(iv));
-  let encrypted = cipher.update(text, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
-  return encrypted;
+  const iv = crypto.randomBytes(16); // Vector khởi tạo
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(secretKey), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
 }
 
 const convertToObjectIdMongodb = (id) => new Types.ObjectId(id);
