570d4fecd646d842c2a2dbdbd57a59f1a981583f
thuc-nguyen <hoangthuc701@gmail.com>
2024-05-12 22:54:46 +0700

    encrypt answer

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,18 @@
 
 const _ = require("lodash");
 const { Types } = require("mongoose");
+const crypto = require('crypto');
+
+const algorithm = 'aes-256-cbc';
+const key = '12345678901234567890123456789012'; // Khóa 32 bytes cho aes-256
+const iv = '1234567890123456'; // IV 16 bytes
+
+function encrypt(text) {
+  let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), Buffer.from(iv));
+  let encrypted = cipher.update(text, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return encrypted;
+}
 
 const convertToObjectIdMongodb = (id) => new Types.ObjectId(id);
 
@@ -12,4 +24,5 @@ const getInfoData = ({ fileds = [], object = {} }) => {
 module.exports = {
   getInfoData,
   convertToObjectIdMongodb,
+  encrypt
 };
